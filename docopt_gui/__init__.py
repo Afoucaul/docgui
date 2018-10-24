4da96ab019cc0e879c10fbfc356a89d4ffaab535
armand <armand.foucault@telecom-bretagne.eu>
2018-10-24 11:15:48 +0200

    Imported modules in __init__

--- docopt_gui/__init__.py
+++ docopt_gui/__init__.py
@@ -2,3 +2,5 @@ from importlib import import_module
 
 
 usage_parser = import_module(".usage_parser", __name__)
+application = import_module(".application", __name__)
+utils = import_module(".utils", __name__)
