a0a85454cfb46ca828cbb2581122c5af11830546
armand <armand.foucault@telecom-bretagne.eu>
2018-10-24 11:16:14 +0200

    Wrote class CommandWrapper

--- docopt_gui/usage_parser.py
+++ docopt_gui/usage_parser.py
@@ -1,13 +1,46 @@
-from docopt import parse_pattern, formal_usage, printable_usage
+from docopt import (
+    parse_pattern, 
+    formal_usage, 
+    printable_usage, 
+    Command, 
+    Pattern, 
+    Argument, 
+    Required,
+    Option
+)
 
 
 def get_commands(doc):
-    parsed = parse_pattern(formal_usage(printable_usage(doc)), [])
+    patterns = parse_pattern(formal_usage(printable_usage(doc)), []).children[0]
 
-    commands = []
-    for command in parsed.children[0].children:
-        children = command.children
-        if children:
-            commands.append(children[0])
+    for r in patterns.flat(Required):
+        if not any(isinstance(child, Command) for child in r.children):
+            r.children.insert(0, Command(''))
 
-    return commands
+    return [CommandWrapper(
+                cmd.flat(Command)[0].name, 
+                cmd.flat(Argument), 
+                cmd.flat(Option)) 
+            for cmd in patterns.flat(Required)]
+
+
+def walk_tree(tree, function):
+    if hasattr(tree, 'children'):
+        function(tree)
+        [function(c) for c in tree.children]
+
+    elif isinstance(tree, list):
+        [function(c) for c in tree]
+
+    else:
+        function(tree)
+
+
+class CommandWrapper:
+    def __init__(self, name, args, options):
+        self.name = name
+        self.args = args
+        self.options = options
+
+    def __repr__(self):
+        return "CommandWrapper('%s', %r)" % (self.name, self.args)
